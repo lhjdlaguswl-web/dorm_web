fd3875fa6d0d2094989eec9ac3949657fa4c7a9a
lhjdlaguswl-web <lhjdlaguswl@gmail.com>
2026-06-20 16:18:14 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Supabase 연결
 const SUPABASE_URL = 'https://cfjkgbyrzmkfqsgbrbsx.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmamtnYnlyem1rZnFzZ2JyYnN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzI3OTMsImV4cCI6MjA5NzUwODc5M30.tZnUoUGyObV7YLBZnKPSZynoPJAe9KrmyW0eynqtKZQ'
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
 // 로그인
 async function login() {
