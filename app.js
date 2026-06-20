5ac6d6e2058b99369c68805091d51958ba38b1d9
lhjdlaguswl-web <lhjdlaguswl@gmail.com>
2026-06-20 15:58:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-function addReport() {
-  const title = document.getElementById("title").value;
-  const location = document.getElementById("location").value;
-  const desc = document.getElementById("desc").value;
+// Supabase 연결
+const SUPABASE_URL = 'https://cfjkgbyrzmkfqsgbrbsx.supabase.co/rest/v1/'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNmamtnYnlyem1rZnFzZ2JyYnN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzI3OTMsImV4cCI6MjA5NzUwODc5M30.tZnUoUGyObV7YLBZnKPSZynoPJAe9KrmyW0eynqtKZQ
 
-  const list = document.getElementById("list");
+'
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
-  const item = document.createElement("li");
-  item.innerText = `${title} | ${location} | ${desc}`;
+// 로그인
+async function login() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
 
-  list.appendChild(item);
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  })
 
-  // 입력창 초기화
-  document.getElementById("title").value = "";
-  document.getElementById("location").value = "";
-  document.getElementById("desc").value = "";
+  if (error) {
+    alert('로그인 실패: ' + error.message)
+    return
+  }
+
+  alert('로그인 성공!')
+  window.location.href = 'dashboard.html'
 }
